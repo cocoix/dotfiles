71bcab00bb557a8d131a92cb3ff816c5ad6006a3
cocoix <cocoix00@gmail.com>
2025-11-11 23:12:14 +0800

    update: nvim

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -16,4 +16,15 @@ return {
             no_underline = false, -- Force no underline
         },
     },
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.gruvbox_material_enable_italic = true
+            -- vim.cmd.colorscheme('gruvbox-material')
+        end
+    }
 }
