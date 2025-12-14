5cdde6e0fa9348683301e53fedfcf39efe763164
cocoix <cocoix00@gmail.com>
2025-12-14 22:19:36 +0800

    修改 wezterm 颜色

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,16 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
--- config.color_scheme = 'Everforest Dark Hard (Gogh)'
 config.color_scheme = 'Gruvbox Material (Gogh)'
+config.colors = {
+    background = '#202020'
+}
 config.font = wezterm.font_with_fallback {
     'Maple Mono Normal NL NF CN',
-    'Mononoki Nerd Font Mono',
     'font-sarasa-gothic',
 }
 config.font_size = 14
+config.line_height = 1.2
 config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = 'BlinkingBar'
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
